e5cba206f2d12fd069e5306e20703b2fc4aa4bf4
Jie Xun <jxsee94@yahoo.com.sg>
2017-01-14 05:10:03 +0800

    master

--- app.js
+++ app.js
@@ -79,19 +79,21 @@ bot.dialog('/sayHi', [
             var lat = session.message.entities[0].geo.latitude;
             var lon = session.message.entities[0].geo.longitude;
             session.endDialog(lat+", "+lon);
-            var url = "https://developers.onemap.sg/privateapi/themesvc/retrieveTheme?queryName=recyclingbins&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI4MSwidXNlcl9pZCI6MjgxLCJlbWFpbCI6Im9uZ2ppYXJ1aUBob3RtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC8xMC4wLjMuMTE6ODA4MFwvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTQ4NDI4Mzk1NCwiZXhwIjoxNDg0NzE1OTU0LCJuYmYiOjE0ODQyODM5NTQsImp0aSI6IjIxYjhlODgxODQ1MmVlODVkZmU2NjRlOTU1YjI5M2I4In0.E7DM-ism_4Vt6JE4zElfsC6-QhAsldmPSGuMZH9AvgQ&extents=1.291789,%20103.7796402,1.3290461,%20103.8726032&extents";
+            var upplat = lat+0.01;
+            var lowlat = lat-0.01;
+            var upplon = lon+0.01;
+            var lowlon = lon-0.01;
+            var url = "https://developers.onemap.sg/privateapi/themesvc/retrieveTheme?queryName=recyclingbins&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjI4MSwidXNlcl9pZCI6MjgxLCJlbWFpbCI6Im9uZ2ppYXJ1aUBob3RtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC8xMC4wLjMuMTE6ODA4MFwvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTQ4NDI4Mzk1NCwiZXhwIjoxNDg0NzE1OTU0LCJuYmYiOjE0ODQyODM5NTQsImp0aSI6IjIxYjhlODgxODQ1MmVlODVkZmU2NjRlOTU1YjI5M2I4In0.E7DM-ism_4Vt6JE4zElfsC6-QhAsldmPSGuMZH9AvgQ&extents="+lowlat+",%20"+lowlon+","+upplat+",%20"+upplon;
             // Build options for the request
             var options = {
                 uri: url,
-                // headers: {
-                //     'Ocp-Apim-Subscription-Key': BINGNEWSKEY
-                // },
                 json: true // Returns the response in json
             }
             //Make the call
                 rp(options).then(function (body){
                     // The request is successful
                     console.log(body);
+                    session.send(body.NAME);
                     //sendTopNews(session, results, body);
                 }).catch(function (err){
                     // An error occurred and the request failed

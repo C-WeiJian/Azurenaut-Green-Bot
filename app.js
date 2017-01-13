1e456c46dddb677ba794aa3aa995197857120750
Jie Xun <jxsee94@yahoo.com.sg>
2017-01-14 04:13:59 +0800

    master

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ bot.dialog('/sayHi', [
     },
     function (session, results) {
         session.send("getting coordinates");
-        session.send(results.response);
+        session.send(results.latitude);
         if(results.messageobj.type=='location'){
             session.send("location received");
             var lat = results.messageobj.latitude;
